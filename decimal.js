8083c037713af012b9dfaabb5a7f94f4f0c43410
daiidie <delavegae46@gmail.com>
2021-02-17 09:21:51 +0100

    radix

---
+++ decimal.js
@@ -0,0 +1,20 @@
+let nUmber1 = 0x123
+console.log(nUmber1.toString([10]))
+
+let nUmber2 = 0123
+console.log(nUmber2.toString([10]))
+
+let nUmber3 = 0b10011001
+console.log(nUmber3.toString([10]))
+
+let nUmber4 = 0xdeadbeef
+console.log(nUmber4.toString([10]))
+
+let nUmber5 = 0xea7beef
+console.log(nUmber5.toString([10]))
+
+let nUmber6 = 0b1111111111111111
+console.log(nUmber6.toString([10]))
+
+let nUmber7 = 0777
+console.log(nUmber7.toString([10]))
