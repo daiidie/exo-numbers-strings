a4aaf3118195455d86e3cccaf236ed7bcab162f9
daiidie <delavegae46@gmail.com>
2021-02-17 09:31:09 +0100

    first

---
+++ convert.js
@@ -0,0 +1,26 @@
+let nb1 = 10
+console.log(nb1.toString(2))
+console.log(nb1.toString(8))
+console.log(nb1.toString(16))
+
+let nb2 = 15
+console.log(nb2.toString(2))
+console.log(nb2.toString(8))
+console.log(nb2.toString(16))
+
+let nb3 = 16
+console.log(nb3.toString(2))
+console.log(nb3.toString(8))
+console.log(nb3.toString(16))
+
+let nb4 = 5005
+console.log(nb4.toString(2))
+console.log(nb4.toString(8))
+console.log(nb4.toString(16))
+
+let nb5 = 52390903
+console.log(nb4.toString(2))
+console.log(nb4.toString(8))
+console.log(nb4.toString(16))
+
+
