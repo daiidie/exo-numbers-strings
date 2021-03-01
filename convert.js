95770582aadb2b704311e387d36e9eb28cd6c298
daiidie <delavegae46@gmail.com>
2021-03-01 18:52:04 +0100

    fix

--- convert.js
+++ convert.js
@@ -1,26 +1,27 @@
 let nb1 = 10
 console.log(nb1.toString(2))
 console.log(nb1.toString(8))
-console.log(nb1.toString(16))
-
+console.log(nb1.toString(16))  
+console.log('\n')
 let nb2 = 15
 console.log(nb2.toString(2))
 console.log(nb2.toString(8))
-console.log(nb2.toString(16))
-
+console.log(nb2.toString(16))  
+console.log('\n')
 let nb3 = 16
-console.log(nb3.toString(2))
+console.log(nb3.toString(2))  
 console.log(nb3.toString(8))
-console.log(nb3.toString(16))
-
+console.log(nb3.toString(16))  
+console.log('\n')
 let nb4 = 5005
 console.log(nb4.toString(2))
 console.log(nb4.toString(8))
-console.log(nb4.toString(16))
-
+console.log(nb4.toString(16))  
+console.log('\n')
 let nb5 = 52390903
 console.log(nb4.toString(2))
 console.log(nb4.toString(8))
-console.log(nb4.toString(16))
+console.log(nb4.toString(16))  
+
 
 
